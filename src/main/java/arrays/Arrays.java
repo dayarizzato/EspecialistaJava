2db67287ed8c98b03e87e01761638315c3d7f557
Dayana Rizzato <dayanarizzato@gmail.com>
2024-10-14 21:54:31 -0300

    Arrays

---
+++ src/main/java/arrays/Arrays.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+
+        //usado para quando nao quero que inicialize com algum valor
+        //int[] notas1 = new int[4];
+
+        //usado para quando quero atribuir valores no array
+        int[] notas = new int[]{10, 33, 56, 88};
+    }
+
+}
