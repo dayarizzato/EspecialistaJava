ef82a03823ffdd9cf443bc31ff22e5d976395805
Dayana Rizzato <dayanarizzato@gmail.com>
2024-10-16 23:08:42 -0300

    Arrays

--- src/main/java/arrays/iterandoComArrays/Principal.java
+++ src/main/java/arrays/iterandoComArrays/Principal.java
@@ -1,10 +1,14 @@
 package arrays.iterandoComArrays;
 
+import java.util.Arrays;
+
 public class Principal {
     public static void main(String[] args) {
         double[] notas = {2,4,6,8};
 
         double media = Calculadora.calcular(notas);
+        String valorNotas = Arrays.toString(notas);
+        System.out.println(valorNotas);
         System.out.println(media);
     }
 }
