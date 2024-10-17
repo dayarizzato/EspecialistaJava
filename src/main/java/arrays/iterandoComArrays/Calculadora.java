435c91c02945670627e696e86ce182ab44865ffe
Dayana Rizzato <dayanarizzato@gmail.com>
2024-10-16 22:55:56 -0300

    Arrays

---
+++ src/main/java/arrays/iterandoComArrays/Calculadora.java
@@ -0,0 +1,11 @@
+package arrays.iterandoComArrays;
+
+public class Calculadora {
+    public static double calcular(double numero[]){
+        double total = 0;
+        for (int i = 0; i < numero.length; i++) {
+            total += numero[i];
+        }
+        return total / numero.length;
+    }
+}
---
+++ src/main/java/arrays/iterandoComArrays/Principal.java
@@ -0,0 +1,10 @@
+package arrays.iterandoComArrays;
+
+public class Principal {
+    public static void main(String[] args) {
+        double[] notas = {2,4,6,8};
+
+        double media = Calculadora.calcular(notas);
+        System.out.println(media);
+    }
+}
