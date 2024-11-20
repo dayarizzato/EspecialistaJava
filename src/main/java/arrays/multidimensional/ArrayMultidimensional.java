d691ccf47e4ebd2a6412ed3b3940916dc1e58758
Dayana Rizzato <dayanarizzato@gmail.com>
2024-11-20 19:59:51 -0300

    s

---
+++ src/main/java/arrays/multidimensional/ArrayMultidimensional.java
@@ -0,0 +1,18 @@
+package arrays.multidimensional;
+
+import java.util.Arrays;
+
+public class ArrayMultidimensional {
+    public static void main(String[] args) {
+        //linha, coluna
+        String[][] cidades = new String[3][3];
+        cidades[0][0] = "Barueri";
+        cidades[0][1] = "São Paulo";
+        cidades[0][2] = "Cotia";
+
+        cidades[1][0] = "Osasco";
+        cidades[1][1] = "Birigui";
+        cidades[1][2] = "Brauna";
+
+    }
+}
